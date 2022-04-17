893a0a13674bd730990d4fa05a064782520de190
qumn <2476573497@qq.com>
2022-04-17 10:14:23 +0800

    add scratch

--- main/rules.lua
+++ main/rules.lua
@@ -74,7 +74,8 @@ function _M.get(clientkeys, clientbuttons)
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
-
+    { rule = { instance = "scratch" },
+          properties = { floating = true} },
   }
 
   return rules
