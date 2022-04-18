1e2ef84a86b2092c77e6bc7626a9da2f608c7b54
qumn <2476573497@qq.com>
2022-04-18 19:17:28 +0800

    delete win+r win+x keymap

--- binding/globalkeys.lua
+++ binding/globalkeys.lua
@@ -115,22 +115,10 @@ function _M.get()
 
     --   -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
     awful.key({ modkey },             "i",    function() awful.spawn("rofi -show drun") end,
               { description = "rofi", group = "launcher" }),
     awful.key({ modkey, },            "w",    function() awful.spawn("rofi -show window -theme ") end,
               { description = "show all windows from all workspaces", group = "awesome" }),
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
 
     --   -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
     -- Resize
